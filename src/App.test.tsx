88c92257100843d808e8df8d9280c4398164ac36
GabrielAlbanez <gabriel.g.abanez@gmail.com>
2023-09-11 09:44:54 -0300

    teste feitos

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,26 +1,77 @@
-import {render,screen,fireEvent, getByText} from "@testing-library/react"
-import App from "./App"
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import App from "./App";
 
-describe('App component',()=>{
-  it('should render App component',()=>{
-    render(<App/>)
-    screen.getByText('Agentes')
-  })
-  it('should filter agents',()=>{
-    render(<App/>)
+describe("App component", () => {
+  //abaixo estamos realizado um mock de uma api ou seja uma chamada fake
 
-    const filterInput = screen.getByPlaceholderText('Filtrar por nome')
-    fireEvent.change(filterInput,{target : {value : 'Gekko'}})
-    
-    
-    const buttonFilter = screen.getByText('Filtrar')
-    fireEvent.click(buttonFilter)
+  const server = setupServer(
+    rest.get("https://valorant-api.com/v1/agents", async (req, res, ctx) => {
+      return res(
+        ctx.json({
+          data: [
+            {
+              uuid: "e370fa57-4757-3604-3648-499e1f642d3f",
+              displayName: "Gekko",
+              displayIcon:
+                "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/displayicon.png",
+            },
+            // Add more agents as needed
+          ],
+        })
+      );
+    })
+  );
 
+  //o beforeAll server para usar esse mock em todos nossos teste
+  beforeAll(() => server.listen());
+  //temos que usar o resethandles pq para cada teste tenho seu handler propio
+  //o teste abaixo mostra como fazer isso basta usar o server.use() os testes que não tem isso
+  // usa o mock inicial para os testes
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
 
+  it("should show error message", async () => {
+    //teste server para demostrar se a api retornar um erro 500
+    //aq estamos formçando um mock de erro 500 assim retorna a messagem la no front end
+    //e como ele acho a message da para mostrar o erro 
+    // mas temos que levar que isso é fake pois a api do valorant n ta retornando um erro 500
+    server.use(
+      rest.get("https://valorant-api.com/v1/agents",  (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({ message: "error " }));
+      })
+    );
+    render(<App />);
+    await screen.findByText("Erro na requisição à API");
+  });
+  //estamos testando se a api ta retornando os dados e ela esta pois ele esta achando o nome do agente
+  it("should fetch and show agents after component is rendered", async () => {
+    render(<App />);
+    // Wait for the "Gekko" text to be present in the document
+    await screen.findByText("Gekko");
+  });
 
+  it("should render App component", () => {
+    render(<App />);
+    expect(screen.getByText("Agentes")).toBeInTheDocument();
+  });
 
+  it("should write in the filter", () => {
+    render(<App />);
+    const filterInput = screen.getByPlaceholderText("Filtrar por nome");
 
+    fireEvent.change(filterInput, { target: { value: "Gekko" } });
 
+    // You can add more assertions or interactions as needed
+  });
 
-  })
-})
+  it("should click button filter", () => {
+    render(<App />);
+    const buttonFilter = screen.getByText("Filtrar");
+
+    fireEvent.click(buttonFilter);
+
+    // You can add more assertions or interactions as needed
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import "./App.css"
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 interface Agent {
   uuid?: number;
@@ -8,29 +8,33 @@ interface Agent {
 }
 
 const AgentCard: React.FC<Agent> = ({ displayName, displayIcon }) => (
-  <div className="bg-white rounded-lg shadow-md p-4 m-4">
+  <div className=" bg-gradient-to-br from-black via-red-600 to-red-900 rounded-lg shadow-xl p-4 m-4">
     <img src={displayIcon} alt={displayName} className="w-32 h-32 mx-auto" />
-    <p className="text-center mt-2">{displayName}</p>
+    <p className="text-center mt-2 text-white font-semibold">{displayName}</p>
   </div>
 );
 
 const App: React.FC = () => {
   const [agents, setAgents] = useState<Agent[]>([]);
   const [filteredAgents, setFilteredAgents] = useState<Agent[]>([]);
-  const [filterName, setFilterName] = useState<string>('');
+  const [filterName, setFilterName] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<null | string>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://valorant-api.com/v1/agents');
+        const response = await fetch("https://valorant-api.com/v1/agents", {
+          method: "GET",
+        });
         if (!response.ok) {
-          throw new Error('Erro na requisição à API');
+          throw new Error("Erro na requisição à API");
         }
         const data = await response.json();
         setAgents(data.data);
         setFilteredAgents(data.data);
-      } catch (error) {
-        console.error('Erro na requisição à API:', error);
+        setErrorMessage(null);
+      } catch (error: any) {
+        setErrorMessage(error?.message);
       }
     };
 
@@ -40,35 +44,43 @@ const App: React.FC = () => {
   const handleFilter = () => {
     const filtered = agents.filter((agent) =>
       agent.displayName.toLowerCase().includes(filterName.toLowerCase())
+      //se ambas as strings que foram convertidas para minusculas baterem retorna true assim dando o fiter no array e o arry filtrado é guardado no filtered que vc seta o state fitleredAgents
     );
     setFilteredAgents(filtered);
   };
 
   return (
-    <div className="container mx-auto p-4">
-        <h1>Agentes</h1>
+    <div className="container  mx-auto p-4 ">
+      <h1 className="text-center text-4xl text-blue-600 mb-6">Agentes</h1>
 
-      <div className='w-[100%] flex items-center justify-center '>
-      <input
-        type="text"
-        placeholder="Filtrar por nome"
-        value={filterName}
-        onChange={(e) => setFilterName(e.target.value)}
-        className="p-2 border rounded-lg mr-2"
-      />
-      <button onClick={handleFilter} className="px-4 py-2 bg-blue-500 text-white rounded-lg">
-        Filtrar
-      </button>
+      <div className="w-[100%] flex items-center justify-center mb-4">
+        <input
+          type="text"
+          placeholder="Filtrar por nome"
+          value={filterName}
+          onChange={(e) => setFilterName(e.target.value)}
+          className="p-2 border rounded-lg mr-2"
+        />
+        <button
+          onClick={handleFilter}
+          className="px-4 py-2 bg-purple-600 text-white rounded-lg"
+        >
+          Filtrar
+        </button>
       </div>
-      <div className="grid grid-cols-2 gap-4 mt-4">
-        {filteredAgents.map((agent) => (
-          <AgentCard
-            key={agent.uuid}
-            displayName={agent.displayName}
-            displayIcon={agent.displayIcon}
-          />
-        ))}
-      </div>
+      {filteredAgents?.length > 0 ? (
+        <div className="grid grid-cols-2 gap-4j ustify-items-center">
+          {filteredAgents.map((agent) => (
+            <AgentCard
+              key={agent.uuid}
+              displayName={agent.displayName}
+              displayIcon={agent.displayIcon}
+            />
+          ))}
+        </div>
+      ) : (
+        <h1 className="text-center text-red-600">{errorMessage}</h1>
+      )}
     </div>
   );
 };
